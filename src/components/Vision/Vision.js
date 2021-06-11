87ac0932ec49a615bdeeeb53dda8afb893971a77
Dylan Wiafe <dylanwiafe@gmail.com>
2021-06-11 11:14:04 -0400

    components made

--- src/components/Vision/Vision.js
+++ src/components/Vision/Vision.js
@@ -3,13 +3,16 @@ import "/Vision.scss";
 class Vision extends Component {
   render() {
     return (
-      <div>
-        <div className="">
-          <img className="" src="" alt="" />
+      <div className="vision">
+        <div className="vision__col--photo">
+          <img className="vision__img" src="" alt="" />
         </div>
-        <div className="">
-          <h2 className=""></h2>
-          <p className=""></p>
+        <div className="vision__col--copy">
+          <h2 className="vision__heading">lorem ipsum</h2>
+          <p className="vision__copy">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+            similique ipsum repellat excepturi modi!.
+          </p>
         </div>
       </div>
     );
