9e1f13ae9a110fafd082b899f4f0cb90b207652d
Dylan Wiafe <dylanwiafe@gmail.com>
2021-06-11 14:20:34 -0400

    styling

--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import "./WhatWeDo.scss";
+import templateImage from "../../assets/images/unit__content__model__1.png";
 class WhatWeDo extends Component {
   render() {
     return (
       <div className="what-we-do">
+        <div className="what-we-do__col--photo">
+          <img
+            className="what-we-do__img"
+            alt="what-we-do"
+            src={templateImage}
+          />
+        </div>
         <div className="what-we-do__col--copy">
           <h2 className="what-we-do__heading">lorem ipsum</h2>
           <p className="what-we-do__copy">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
             similique ipsum repellat excepturi modi!.
           </p>
-        </div>
-        <div className="what-we-do__col--photo">
-          <img className="what-we-do__img" src="" alt="what-we-do-img" />
         </div>
       </div>
     );
--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import Header from "../../components/Header/Header"
-import Hero from "../../components/Hero/Hero"
-import Vision from "../../components/Vision/Vision"
-import Values from "../../components/Values/Values"
-import WhatWeDo from '../../components/WhatWeDo/WhatWeDo'
-import Endorsements from "../../components/Endorsements/Endorsements"
-import AppStore from "../../components/AppStore/AppStore"
-import Footer from "../../components/Footer/Footer"
-
+import React from "react";
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import Vision from "../../components/Vision/Vision";
+import Values from "../../components/Values/Values";
+import WhatWeDo from "../../components/WhatWeDo/WhatWeDo";
+import Endorsements from "../../components/Endorsements/Endorsements";
+import AppStore from "../../components/AppStore/AppStore";
+import Footer from "../../components/Footer/Footer";
 
 function Homepage() {
-    return (
-        <div>
-            <Header />
-            <Hero />
-            <Vision />
-            <Values />
-            <WhatWeDo />
-            <Endorsements />
-            <AppStore />
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="HomePage">
+      <Header />
+      <Hero />
+      <Vision />
+      <Values />
+      <WhatWeDo />
+      <Endorsements />
+      <AppStore />
+      <Footer />
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
