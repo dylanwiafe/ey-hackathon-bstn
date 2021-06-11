61cf9c86027a6378cf82de69ff188b68789f6ac6
Dylan Wiafe <dylanwiafe@gmail.com>
2021-06-11 00:05:36 -0400

    Vision, Values, What We Do Outline

---
+++ src/components/Vision/Vision.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "/Vision.scss";
+class Vision extends Component {
+  render() {
+    return (
+      <div>
+        <div className="">
+          <img className="" src="" alt="" />
+        </div>
+        <div className="">
+          <h2 className=""></h2>
+          <p className=""></p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Vision;
